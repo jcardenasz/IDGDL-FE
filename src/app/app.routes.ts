3032d0cc9f5f917e9dc68df2396e329edcf2137a
jcardenasz <jcardenasz@unal.edu.co>
2025-06-05 14:56:23 -0500

    feat: component structure navbar ⭕

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home'},
+  { path: 'about'},
+  { path: 'contact'},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
