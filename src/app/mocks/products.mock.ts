c541484de711367c76e5f88096772e017dbf84ba
jcardenasz <jcardenasz@unal.edu.co>
2025-07-05 12:04:50 -0500

    feat: functional and style pagination 🟢

---
+++ src/app/mocks/products.mock.ts
@@ -0,0 +1,711 @@
+export interface ProductMock {
+  id: string;
+  title: string;
+  brand: string;
+  price: number;
+  imgSrc: string;
+}
+
+export const productsMock: ProductMock[] = [
+  {
+    id: '1',
+    title: 'Guantes de Seguridad',
+    brand: 'Marca Industrial',
+    price: 45000,
+    imgSrc: 'https://picsum.photos/300/200?random=1'
+  },
+  {
+    id: '2',
+    title: 'Casco de Protección',
+    brand: 'SafetyPro',
+    price: 75000,
+    imgSrc: 'https://picsum.photos/300/200?random=2'
+  },
+  {
+    id: '3',
+    title: 'Botas de Seguridad',
+    brand: 'WorkSafe',
+    price: 120000,
+    imgSrc: 'https://picsum.photos/300/200?random=3'
+  },
+  {
+    id: '4',
+    title: 'Chaleco Reflectivo',
+    brand: 'VisibilityPlus',
+    price: 35000,
+    imgSrc: 'https://picsum.photos/300/200?random=4'
+  },
+  {
+    id: '5',
+    title: 'Gafas de Protección',
+    brand: 'ClearVision',
+    price: 25000,
+    imgSrc: 'https://picsum.photos/300/200?random=5'
+  },
+  {
+    id: '6',
+    title: 'Mascarilla N95',
+    brand: 'AirSafe',
+    price: 15000,
+    imgSrc: 'https://picsum.photos/300/200?random=6'
+  },
+  {
+    id: '7',
+    title: 'Arnés de Seguridad',
+    brand: 'HeightSafe',
+    price: 180000,
+    imgSrc: 'https://picsum.photos/300/200?random=7'
+  },
+  {
+    id: '8',
+    title: 'Tapones para Oídos',
+    brand: 'SoundBlock',
+    price: 8000,
+    imgSrc: 'https://picsum.photos/300/200?random=8'
+  },
+  {
+    id: '9',
+    title: 'Overol de Trabajo',
+    brand: 'WorkWear',
+    price: 95000,
+    imgSrc: 'https://picsum.photos/300/200?random=9'
+  },
+  {
+    id: '10',
+    title: 'Extintor Portátil',
+    brand: 'FireSafe',
+    price: 65000,
+    imgSrc: 'https://picsum.photos/300/200?random=10'
+  },
+  {
+    id: '11',
+    title: 'Kit de Primeros Auxilios',
+    brand: 'MedSafe',
+    price: 85000,
+    imgSrc: 'https://picsum.photos/300/200?random=11'
+  },
+  {
+    id: '12',
+    title: 'Señalización de Seguridad',
+    brand: 'SignalPro',
+    price: 12000,
+    imgSrc: 'https://picsum.photos/300/200?random=12'
+  },
+  {
+    id: '13',
+    title: 'Casco con Lámpara LED',
+    brand: 'LightTech',
+    price: 95000,
+    imgSrc: 'https://picsum.photos/300/200?random=13'
+  },
+  {
+    id: '14',
+    title: 'Guantes Anticorte',
+    brand: 'CutResist',
+    price: 65000,
+    imgSrc: 'https://picsum.photos/300/200?random=14'
+  },
+  {
+    id: '15',
+    title: 'Respirador Media Cara',
+    brand: 'AirGuard',
+    price: 125000,
+    imgSrc: 'https://picsum.photos/300/200?random=15'
+  },
+  {
+    id: '16',
+    title: 'Botas Dieléctricas',
+    brand: 'ElectroSafe',
+    price: 185000,
+    imgSrc: 'https://picsum.photos/300/200?random=16'
+  },
+  {
+    id: '17',
+    title: 'Protector Facial',
+    brand: 'FaceShield',
+    price: 28000,
+    imgSrc: 'https://picsum.photos/300/200?random=17'
+  },
+  {
+    id: '18',
+    title: 'Linterna de Seguridad',
+    brand: 'BrightWork',
+    price: 42000,
+    imgSrc: 'https://picsum.photos/300/200?random=18'
+  },
+  {
+    id: '19',
+    title: 'Camisa Manga Larga',
+    brand: 'SafeWear',
+    price: 55000,
+    imgSrc: 'https://picsum.photos/300/200?random=19'
+  },
+  {
+    id: '20',
+    title: 'Detector de Gases',
+    brand: 'GasAlert',
+    price: 320000,
+    imgSrc: 'https://picsum.photos/300/200?random=20'
+  },
+  {
+    id: '21',
+    title: 'Rodilleras de Protección',
+    brand: 'KneePro',
+    price: 38000,
+    imgSrc: 'https://picsum.photos/300/200?random=21'
+  },
+  {
+    id: '22',
+    title: 'Cuerda de Seguridad',
+    brand: 'RopeTech',
+    price: 145000,
+    imgSrc: 'https://picsum.photos/300/200?random=22'
+  },
+  {
+    id: '23',
+    title: 'Mascarilla Desechable',
+    brand: 'DispoMask',
+    price: 2500,
+    imgSrc: 'https://picsum.photos/300/200?random=23'
+  },
+  {
+    id: '24',
+    title: 'Gafas Soldadura',
+    brand: 'WeldView',
+    price: 85000,
+    imgSrc: 'https://picsum.photos/300/200?random=24'
+  },
+  {
+    id: '25',
+    title: 'Delantal de Cuero',
+    brand: 'LeatherPro',
+    price: 78000,
+    imgSrc: 'https://picsum.photos/300/200?random=25'
+  },
+  {
+    id: '26',
+    title: 'Conos de Tráfico',
+    brand: 'TrafficSafe',
+    price: 22000,
+    imgSrc: 'https://picsum.photos/300/200?random=26'
+  },
+  {
+    id: '27',
+    title: 'Manta Ignífuga',
+    brand: 'FireStop',
+    price: 156000,
+    imgSrc: 'https://picsum.photos/300/200?random=27'
+  },
+  {
+    id: '28',
+    title: 'Casco Soldador',
+    brand: 'WeldSafe',
+    price: 175000,
+    imgSrc: 'https://picsum.photos/300/200?random=28'
+  },
+  {
+    id: '29',
+    title: 'Guantes Químicos',
+    brand: 'ChemGuard',
+    price: 52000,
+    imgSrc: 'https://picsum.photos/300/200?random=29'
+  },
+  {
+    id: '30',
+    title: 'Chaleco Salvavidas',
+    brand: 'AquaSafe',
+    price: 89000,
+    imgSrc: 'https://picsum.photos/300/200?random=30'
+  },
+  {
+    id: '31',
+    title: 'Escalera Telescópica',
+    brand: 'ClimbSafe',
+    price: 485000,
+    imgSrc: 'https://picsum.photos/300/200?random=31'
+  },
+  {
+    id: '32',
+    title: 'Protector Auditivo',
+    brand: 'HearGuard',
+    price: 18000,
+    imgSrc: 'https://picsum.photos/300/200?random=32'
+  },
+  {
+    id: '33',
+    title: 'Traje Tyvek',
+    brand: 'ChemSuit',
+    price: 125000,
+    imgSrc: 'https://picsum.photos/300/200?random=33'
+  },
+  {
+    id: '34',
+    title: 'Candado de Seguridad',
+    brand: 'LockOut',
+    price: 35000,
+    imgSrc: 'https://picsum.photos/300/200?random=34'
+  },
+  {
+    id: '35',
+    title: 'Banda Reflectiva',
+    brand: 'ReflectMax',
+    price: 8500,
+    imgSrc: 'https://picsum.photos/300/200?random=35'
+  },
+  {
+    id: '36',
+    title: 'Casco Ventilado',
+    brand: 'CoolHead',
+    price: 92000,
+    imgSrc: 'https://picsum.photos/300/200?random=36'
+  },
+  {
+    id: '37',
+    title: 'Guantes Térmicos',
+    brand: 'HeatShield',
+    price: 68000,
+    imgSrc: 'https://picsum.photos/300/200?random=37'
+  },
+  {
+    id: '38',
+    title: 'Kit Derrame Químico',
+    brand: 'SpillStop',
+    price: 285000,
+    imgSrc: 'https://picsum.photos/300/200?random=38'
+  },
+  {
+    id: '39',
+    title: 'Botiquín Portátil',
+    brand: 'FirstAid Pro',
+    price: 65000,
+    imgSrc: 'https://picsum.photos/300/200?random=39'
+  },
+  {
+    id: '40',
+    title: 'Respirador Completo',
+    brand: 'FullMask',
+    price: 450000,
+    imgSrc: 'https://picsum.photos/300/200?random=40'
+  },
+  {
+    id: '41',
+    title: 'Cinturón Herramientas',
+    brand: 'ToolBelt',
+    price: 75000,
+    imgSrc: 'https://picsum.photos/300/200?random=41'
+  },
+  {
+    id: '42',
+    title: 'Alarma Personal',
+    brand: 'AlertMax',
+    price: 45000,
+    imgSrc: 'https://picsum.photos/300/200?random=42'
+  },
+  {
+    id: '43',
+    title: 'Protector Dorsal',
+    brand: 'BackGuard',
+    price: 185000,
+    imgSrc: 'https://picsum.photos/300/200?random=43'
+  },
+  {
+    id: '44',
+    title: 'Gorra con Protección',
+    brand: 'CapSafe',
+    price: 32000,
+    imgSrc: 'https://picsum.photos/300/200?random=44'
+  },
+  {
+    id: '45',
+    title: 'Mangas Protectoras',
+    brand: 'ArmGuard',
+    price: 28000,
+    imgSrc: 'https://picsum.photos/300/200?random=45'
+  },
+  {
+    id: '46',
+    title: 'Ducha de Emergencia',
+    brand: 'EmergencyWash',
+    price: 1250000,
+    imgSrc: 'https://picsum.photos/300/200?random=46'
+  },
+  {
+    id: '47',
+    title: 'Polainas de Seguridad',
+    brand: 'LegShield',
+    price: 42000,
+    imgSrc: 'https://picsum.photos/300/200?random=47'
+  },
+  {
+    id: '48',
+    title: 'Radio de Emergencia',
+    brand: 'CommSafe',
+    price: 165000,
+    imgSrc: 'https://picsum.photos/300/200?random=48'
+  },
+  {
+    id: '49',
+    title: 'Barrera de Contención',
+    brand: 'ContainPro',
+    price: 320000,
+    imgSrc: 'https://picsum.photos/300/200?random=49'
+  },
+  {
+    id: '50',
+    title: 'Kit Bloqueo Energía',
+    brand: 'EnergyLock',
+    price: 195000,
+    imgSrc: 'https://picsum.photos/300/200?random=50'
+  },
+  // New products from 51 to 100
+  {
+    id: '51',
+    title: 'Máscara de Soldadura Automática',
+    brand: 'AutoWeld',
+    price: 285000,
+    imgSrc: 'https://picsum.photos/300/200?random=51'
+  },
+  {
+    id: '52',
+    title: 'Guantes de Soldador',
+    brand: 'WeldMaster',
+    price: 48000,
+    imgSrc: 'https://picsum.photos/300/200?random=52'
+  },
+  {
+    id: '53',
+    title: 'Chaqueta de Cuero Soldador',
+    brand: 'LeatherWeld',
+    price: 165000,
+    imgSrc: 'https://picsum.photos/300/200?random=53'
+  },
+  {
+    id: '54',
+    title: 'Filtro de Respirador P100',
+    brand: 'FilterPro',
+    price: 35000,
+    imgSrc: 'https://picsum.photos/300/200?random=54'
+  },
+  {
+    id: '55',
+    title: 'Casco Forestal',
+    brand: 'ForestSafe',
+    price: 125000,
+    imgSrc: 'https://picsum.photos/300/200?random=55'
+  },
+  {
+    id: '56',
+    title: 'Protector de Pantorrilla',
+    brand: 'LegArmor',
+    price: 55000,
+    imgSrc: 'https://picsum.photos/300/200?random=56'
+  },
+  {
+    id: '57',
+    title: 'Lentes de Seguridad Bifocales',
+    brand: 'BiVision',
+    price: 45000,
+    imgSrc: 'https://picsum.photos/300/200?random=57'
+  },
+  {
+    id: '58',
+    title: 'Botas Antiestaticas',
+    brand: 'StaticFree',
+    price: 145000,
+    imgSrc: 'https://picsum.photos/300/200?random=58'
+  },
+  {
+    id: '59',
+    title: 'Traje Ignífugo',
+    brand: 'FlameResist',
+    price: 385000,
+    imgSrc: 'https://picsum.photos/300/200?random=59'
+  },
+  {
+    id: '60',
+    title: 'Sistema de Comunicación',
+    brand: 'TalkSafe',
+    price: 275000,
+    imgSrc: 'https://picsum.photos/300/200?random=60'
+  },
+  {
+    id: '61',
+    title: 'Medidor de Monóxido',
+    brand: 'CODetect',
+    price: 485000,
+    imgSrc: 'https://picsum.photos/300/200?random=61'
+  },
+  {
+    id: '62',
+    title: 'Casco de Rescate',
+    brand: 'RescueGear',
+    price: 155000,
+    imgSrc: 'https://picsum.photos/300/200?random=62'
+  },
+  {
+    id: '63',
+    title: 'Guantes de Electricista',
+    brand: 'ElectroGrip',
+    price: 85000,
+    imgSrc: 'https://picsum.photos/300/200?random=63'
+  },
+  {
+    id: '64',
+    title: 'Protector de Muñeca',
+    brand: 'WristGuard',
+    price: 25000,
+    imgSrc: 'https://picsum.photos/300/200?random=64'
+  },
+  {
+    id: '65',
+    title: 'Mono Desechable',
+    brand: 'DispoSuit',
+    price: 15000,
+    imgSrc: 'https://picsum.photos/300/200?random=65'
+  },
+  {
+    id: '66',
+    title: 'Zapatos de Seguridad Deportivos',
+    brand: 'SportSafe',
+    price: 195000,
+    imgSrc: 'https://picsum.photos/300/200?random=66'
+  },
+  {
+    id: '67',
+    title: 'Kit de Rescate en Altura',
+    brand: 'HeightRescue',
+    price: 850000,
+    imgSrc: 'https://picsum.photos/300/200?random=67'
+  },
+  {
+    id: '68',
+    title: 'Chaleco Refrigerante',
+    brand: 'CoolVest',
+    price: 125000,
+    imgSrc: 'https://picsum.photos/300/200?random=68'
+  },
+  {
+    id: '69',
+    title: 'Protector de Tobillo',
+    brand: 'AnkleShield',
+    price: 35000,
+    imgSrc: 'https://picsum.photos/300/200?random=69'
+  },
+  {
+    id: '70',
+    title: 'Máscara Facial Completa',
+    brand: 'FullFace',
+    price: 385000,
+    imgSrc: 'https://picsum.photos/300/200?random=70'
+  },
+  {
+    id: '71',
+    title: 'Linterna Frontal LED',
+    brand: 'HeadBeam',
+    price: 65000,
+    imgSrc: 'https://picsum.photos/300/200?random=71'
+  },
+  {
+    id: '72',
+    title: 'Guantes Criogénicos',
+    brand: 'CryoSafe',
+    price: 185000,
+    imgSrc: 'https://picsum.photos/300/200?random=72'
+  },
+  {
+    id: '73',
+    title: 'Pantalón de Seguridad',
+    brand: 'SafePants',
+    price: 85000,
+    imgSrc: 'https://picsum.photos/300/200?random=73'
+  },
+  {
+    id: '74',
+    title: 'Detector de Humo Portátil',
+    brand: 'SmokeAlert',
+    price: 125000,
+    imgSrc: 'https://picsum.photos/300/200?random=74'
+  },
+  {
+    id: '75',
+    title: 'Casco Minero',
+    brand: 'MineSafe',
+    price: 145000,
+    imgSrc: 'https://picsum.photos/300/200?random=75'
+  },
+  {
+    id: '76',
+    title: 'Protector de Codo',
+    brand: 'ElbowGuard',
+    price: 28000,
+    imgSrc: 'https://picsum.photos/300/200?random=76'
+  },
+  {
+    id: '77',
+    title: 'Traje de Protección Química',
+    brand: 'ChemShield',
+    price: 485000,
+    imgSrc: 'https://picsum.photos/300/200?random=77'
+  },
+  {
+    id: '78',
+    title: 'Botas de Goma Industrial',
+    brand: 'RubberWork',
+    price: 95000,
+    imgSrc: 'https://picsum.photos/300/200?random=78'
+  },
+  {
+    id: '79',
+    title: 'Sistema de Ventilación Móvil',
+    brand: 'AirFlow',
+    price: 1850000,
+    imgSrc: 'https://picsum.photos/300/200?random=79'
+  },
+  {
+    id: '80',
+    title: 'Guantes de Inspección',
+    brand: 'InspectGrip',
+    price: 22000,
+    imgSrc: 'https://picsum.photos/300/200?random=80'
+  },
+  {
+    id: '81',
+    title: 'Chaleco Multibolsillos',
+    brand: 'PocketVest',
+    price: 65000,
+    imgSrc: 'https://picsum.photos/300/200?random=81'
+  },
+  {
+    id: '82',
+    title: 'Protector de Hombro',
+    brand: 'ShoulderSafe',
+    price: 45000,
+    imgSrc: 'https://picsum.photos/300/200?random=82'
+  },
+  {
+    id: '83',
+    title: 'Lentes Fotocromáticos',
+    brand: 'AutoTint',
+    price: 85000,
+    imgSrc: 'https://picsum.photos/300/200?random=83'
+  },
+  {
+    id: '84',
+    title: 'Equipo de Aire Autónomo',
+    brand: 'SelfAir',
+    price: 3250000,
+    imgSrc: 'https://picsum.photos/300/200?random=84'
+  },
+  {
+    id: '85',
+    title: 'Casco con Protección Facial',
+    brand: 'FaceHelm',
+    price: 165000,
+    imgSrc: 'https://picsum.photos/300/200?random=85'
+  },
+  {
+    id: '86',
+    title: 'Guantes de Manejo',
+    brand: 'HandleGrip',
+    price: 18000,
+    imgSrc: 'https://picsum.photos/300/200?random=86'
+  },
+  {
+    id: '87',
+    title: 'Protector de Cadera',
+    brand: 'HipShield',
+    price: 55000,
+    imgSrc: 'https://picsum.photos/300/200?random=87'
+  },
+  {
+    id: '88',
+    title: 'Máscara Antipolvo N99',
+    brand: 'DustBlock',
+    price: 25000,
+    imgSrc: 'https://picsum.photos/300/200?random=88'
+  },
+  {
+    id: '89',
+    title: 'Botas de Seguridad Ligeras',
+    brand: 'LightStep',
+    price: 165000,
+    imgSrc: 'https://picsum.photos/300/200?random=89'
+  },
+  {
+    id: '90',
+    title: 'Kit de Señalización Vial',
+    brand: 'RoadSign',
+    price: 185000,
+    imgSrc: 'https://picsum.photos/300/200?random=90'
+  },
+  {
+    id: '91',
+    title: 'Protector de Espalda',
+    brand: 'BackShield',
+    price: 125000,
+    imgSrc: 'https://picsum.photos/300/200?random=91'
+  },
+  {
+    id: '92',
+    title: 'Gafas de Laboratorio',
+    brand: 'LabVision',
+    price: 35000,
+    imgSrc: 'https://picsum.photos/300/200?random=92'
+  },
+  {
+    id: '93',
+    title: 'Traje de Soldador Completo',
+    brand: 'WeldArmor',
+    price: 385000,
+    imgSrc: 'https://picsum.photos/300/200?random=93'
+  },
+  {
+    id: '94',
+    title: 'Detector de Voltaje',
+    brand: 'VoltSense',
+    price: 95000,
+    imgSrc: 'https://picsum.photos/300/200?random=94'
+  },
+  {
+    id: '95',
+    title: 'Casco de Bombero',
+    brand: 'FireHelm',
+    price: 425000,
+    imgSrc: 'https://picsum.photos/300/200?random=95'
+  },
+  {
+    id: '96',
+    title: 'Guantes Antivibración',
+    brand: 'VibeStop',
+    price: 75000,
+    imgSrc: 'https://picsum.photos/300/200?random=96'
+  },
+  {
+    id: '97',
+    title: 'Protector de Cuello',
+    brand: 'NeckGuard',
+    price: 32000,
+    imgSrc: 'https://picsum.photos/300/200?random=97'
+  },
+  {
+    id: '98',
+    title: 'Sistema de Purificación de Aire',
+    brand: 'PureAir',
+    price: 2850000,
+    imgSrc: 'https://picsum.photos/300/200?random=98'
+  },
+  {
+    id: '99',
+    title: 'Botas Impermeables',
+    brand: 'WaterProof',
+    price: 135000,
+    imgSrc: 'https://picsum.photos/300/200?random=99'
+  },
+  {
+    id: '100',
+    title: 'Kit de Seguridad Completo',
+    brand: 'CompleteSafe',
+    price: 850000,
+    imgSrc: 'https://picsum.photos/300/200?random=100'
+  }
+];
--- src/app/pages/productos/productos.ts
+++ src/app/pages/productos/productos.ts
@@ -1,107 +1,63 @@
 import { Component } from '@angular/core';
 import { Card } from '../../components/atoms/card/card';
 import { CardProps } from '../../interfaces/interfaces';
+import { productsMock } from '../../mocks/products.mock';
 
 @Component({
   selector: 'app-productos',
   imports: [Card],
   templateUrl: './productos.html',
-  styleUrl: './productos.css'
+  styleUrl: './productos.css',
 })
-
 export class Productos {
+  products: CardProps[] = productsMock;
 
-  products: CardProps[] = [
-    {
-      id: '1',
-      title: 'Guantes de Seguridad',
-      brand: 'Marca Industrial',
-      price: 45000,
-      imgSrc: 'https://picsum.photos/300/200?random=1'
-    },
-    {
-      id: '2',
-      title: 'Casco de Protección',
-      brand: 'SafetyPro',
-      price: 75000,
-      imgSrc: 'https://picsum.photos/300/200?random=2'
-    },
-    {
-      id: '3',
-      title: 'Botas de Seguridad',
-      brand: 'WorkSafe',
-      price: 120000,
-      imgSrc: 'https://picsum.photos/300/200?random=3'
-    },
-    {
-      id: '4',
-      title: 'Chaleco Reflectivo',
-      brand: 'VisibilityPlus',
-      price: 35000,
-      imgSrc: 'https://picsum.photos/300/200?random=4'
-    },
-    {
-      id: '5',
-      title: 'Gafas de Protección',
-      brand: 'ClearVision',
-      price: 25000,
-      imgSrc: 'https://picsum.photos/300/200?random=5'
-    },
-    {
-      id: '6',
-      title: 'Mascarilla N95',
-      brand: 'AirSafe',
-      price: 15000,
-      imgSrc: 'https://picsum.photos/300/200?random=6'
-    },
-    {
-      id: '7',
-      title: 'Arnés de Seguridad',
-      brand: 'HeightSafe',
-      price: 180000,
-      imgSrc: 'https://picsum.photos/300/200?random=7'
-    },
-    {
-      id: '8',
-      title: 'Tapones para Oídos',
-      brand: 'SoundBlock',
-      price: 8000,
-      imgSrc: 'https://picsum.photos/300/200?random=8'
-    },
-    {
-      id: '9',
-      title: 'Overol de Trabajo',
-      brand: 'WorkWear',
-      price: 95000,
-      imgSrc: 'https://picsum.photos/300/200?random=9'
-    },
-    {
-      id: '10',
-      title: 'Extintor Portátil',
-      brand: 'FireSafe',
-      price: 65000,
-      imgSrc: 'https://picsum.photos/300/200?random=10'
-    },
-    {
-      id: '11',
-      title: 'Kit de Primeros Auxilios',
-      brand: 'MedSafe',
-      price: 85000,
-      imgSrc: 'https://picsum.photos/300/200?random=11'
-    },
-    {
-      id: '12',
-      title: 'Señalización de Seguridad',
-      brand: 'SignalPro',
-      price: 12000,
-      imgSrc: 'https://picsum.photos/300/200?random=12'
-    }
-  ];
+  //pagination
+  currentPage: number = 0;
+  pageSize: number = 10;
+  totalProducts: number = this.products.length;
+  totalPages: number = Math.ceil(this.totalProducts / this.pageSize);
 
-  constructor() {
+  get paginatedProducts(): CardProps[] {
+    const start = this.currentPage * this.pageSize;
+    const end = start + this.pageSize;
+    return this.products.slice(start, end);
   }
 
-  onAddToCart(event: any): void {
-      console.log('Added to cart:', event);
+  goToPage(page: number): void {
+    if (page >= 0 && page < this.totalPages) {
+      this.currentPage = page;
     }
+  }
+
+  nextPage(): void {
+    if (this.currentPage < this.totalPages - 1) {
+      this.currentPage++;
+    }
+  }
+
+  previousPage(): void {
+    if (this.currentPage > 0) {
+      this.currentPage--;
+    }
+  }
+
+  get totalPagesArray(): undefined[] {
+    return Array(this.totalPages);
+  }
+
+  get startItemIndex(): number {
+    return this.currentPage * this.pageSize;
+  }
+
+  get endItemIndex(): number {
+    const end = this.startItemIndex + this.pageSize;
+    return end > this.totalProducts ? this.totalProducts : end;
+  }
+
+  constructor() {}
+
+  onAddToCart(event: any): void {
+    console.log('Added to cart:', event);
+  }
 }
